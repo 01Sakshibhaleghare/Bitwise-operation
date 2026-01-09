18d7221bafdd20bf3d2847f918938c5c373bda4c
01Sakshibhaleghare <sakshibhaleghare00@gmail.com>
2026-01-09 22:43:04 +0530

    Add files via upload

---
+++ Bitwiseoperator.cpp
@@ -0,0 +1,24 @@
+//& and | and ^
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int a = 3, b = 7;
+
+//     // cout << (a & b) << endl; 
+//     // cout << (a | b) << endl; 
+//     // cout << (a ^ b) << endl; 
+
+//     return 0;
+// }
+
+// Left shift and Right shift 
+#include <iostream>
+using namespace std;
+
+int main(){
+    //cout << (10 << 2) <<endl;     // left shift
+    cout << (10 >> 2) << endl;
+    return 3;
+}
